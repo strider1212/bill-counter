5bffb8bf847e77a2c3a63f5d38f68be2760cbe3b
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2023-05-07 16:34:17 -0400

    render bills to UI from stringBills

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 import { BillCounter } from "./utils/BillCounter.js";
 import { RenderAsDollarStrings } from "./utils/RenderAsDollarStrings.js";
 
-const bills = BillCounter(257)
+const bills = BillCounter(257);
 
-const stringBills = RenderAsDollarStrings(bills)
+const stringBills = RenderAsDollarStrings(bills);
+
+const list = document.getElementById("bill-list");
+
+stringBills.forEach(item => {
+  const listItem = document.createElement("li");
+  listItem.textContent = item;
+  list.appendChild(listItem);
+})
 
 console.log(stringBills)
