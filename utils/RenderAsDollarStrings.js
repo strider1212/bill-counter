458e306a16b1c030758278ed7e57b4233cacfbf2
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2023-05-07 16:31:53 -0400

    Updated RenderDollarStrings() to show the amount of times a bill is used

--- utils/RenderAsDollarStrings.js
+++ utils/RenderAsDollarStrings.js
@@ -1,9 +1,14 @@
-export const RenderAsDollarStrings = list => {
-  let newBillsList = [];
+export const RenderAsDollarStrings = bills => {
+  let billCounts = new Map();
+  bills.forEach(bill => {
+    billCounts.set(bill, (billCounts.get(bill) || 0) + 1)
+  });
 
-  for (let i = 0; i < list.length; i++) {
-    newBillsList.push(`$${list[i]}`)
-  }
+  const stringBills = [];
+  billCounts.forEach((count, bill) => {
+    const billString = count > 1 ? `$${bill} (x${count})` : `$${bill}`;
+    stringBills.push(billString);
+  });
 
-  return newBillsList;
+  return stringBills;
 }
