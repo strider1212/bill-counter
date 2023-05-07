911aa084b51884373b64bb7adfd0396781ca56ed
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2023-05-07 15:13:16 -0400

    added dollar bills to rendered bill

--- utils/RenderList.js
+++ utils/RenderList.js
@@ -1,9 +1,11 @@
+
+
 export const RenderList = (bills) => {
   const list = document.getElementById("bill-list");
 
   bills.forEach(bill => {
     const listItem = document.createElement("li");
-    listItem.textContent = bill;
+    listItem.textContent = `$${bill}`;
     list.appendChild(listItem);
   })
 
