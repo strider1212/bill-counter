19fc142e9ee96d633fc911d0c09dc0cf91c68c65
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2023-05-07 14:45:43 -0400

    billCounter works

--- main.js
+++ main.js
@@ -1,24 +1,42 @@
 const billCounter = (target) => {
-  let waysToCount = [];
-  let counter = 0;
+  let billList = [];
+  
+  const billListTotal = () => {
+    let count = 0;
+    billList.map(item => {
+      count += item;
+    })
+    return count;
+  }
 
-  const find = (bill, history) => {
-    counter += bill;
+  const find = () => {
 
-    if (counter == target) {
-      waysToCount.push(history)
-    } else if (counter > target) {
+    if (billListTotal() + 100 <= target) {
+      billList.push(100);
+    } else if (billListTotal() + 50 <= target) {
+      billList.push(50);
+    } else if (billListTotal() + 20 <= target) {
+      billList.push(20);
+    } else if (billListTotal() + 10 <= target) {
+      billList.push(10);
+    } else if (billListTotal() + 5 <= target) {
+      billList.push(5);
+    } else if (billListTotal() + 1 <= target) {
+      billList.push(1);
+    }
+
+    if (billListTotal() === target) {
+      return 
+    } else if (billListTotal() > target) {
       return null;
     } else {
-      return  find(10, history !== '' ? `${history} + $${bill}` : '$10') ||
-              find(5, history !== '' ? `${history} + $${bill}` : '$5') ||
-              find(1, history !== '' ? `${history} + $${bill}` : '$1')
+      find();
     }
   }
 
-  find(0, "")
+  find();
 
-  return waysToCount;
+  return billList;
 }
 
-console.log(billCounter(10))
+console.log(billCounter(547))
